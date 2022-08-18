6367cae79059cded54fa2c395f794376b5bbf05c
Vaibhavihole31 <itspinki05@gmail.com>
2022-08-18 08:14:22 +0530

    feat: Multiple doubt validation added

--- client/src/components/DoubtCard/DoubtCard.js
+++ client/src/components/DoubtCard/DoubtCard.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./DoubtCard.css"
 
-function DoubtCard(props) {
-  function getTeachingAssistant(){
+const COURSE_NAME_MAP = {
+  "icp": "Intership Cohort Program",
+  "c": "C Programming",
+  "cpp": "C++ Programming",
+  "python": "Python Programming"
+}
 
-  }
+function DoubtCard(props) {
   return (
     <div className={`card card-recent-doubt ${props.status}-doubt`}>
       <span className={`doubt-card-status-badge ${props.status}-bg`}>{props.status}</span>
@@ -13,7 +17,7 @@ function DoubtCard(props) {
         {props.description}
       </div>
       <p><i className="fa-solid fa-calendar-day"></i> {props.slot}</p>
-      <p><i className="fa-solid fa-graduation-cap"></i> {props.courseName}</p>
+      <p><i className="fa-solid fa-graduation-cap"></i> {COURSE_NAME_MAP[props.courseName]}</p>
       <p><i className="fa-solid fa-chalkboard-user"></i> Teaching Assistant: <b>{props.taName} ({props.taMobile})</b></p>
     </div>
   )
