878c6e3f9ac593125be21baa6055341552fbcc71
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2022-08-21 22:40:38 +0530

    added update status functionality

--- client/src/components/TADoubtCard/TADoubtCard.js
+++ client/src/components/TADoubtCard/TADoubtCard.js
@@ -1,31 +1,68 @@
-import React from 'react';
-import './../../styles/doubtCard.css'
-import './TADoubtCard.css';
-import { COURSE_NAME_MAP } from './../../utils/subjects'
+import React from "react";
+import "./../../styles/doubtCard.css";
+import "./TADoubtCard.css";
+import { COURSE_NAME_MAP } from "./../../utils/subjects";
+import axios from "axios";
 
 function TADoubtCard(props) {
   const { doubt } = props;
+
+  async function updateDoubtStatus(status) {
+    const response = await axios.post(`/updatedoubt`, {
+      doubtId: doubt._id,
+      status: status,
+    });
+    if(response){
+        window.location.reload();
+    }
+  }
+
+  function GetStatusButton(){
+    if(doubt.status === "pending"){
+      return (
+        <button className="btn btn-warning btn-sm" type="button"
+                onClick={(e)=>{updateDoubtStatus("attended")}}>
+                <i class="fa-solid fa-video"></i> Mark Attended
+        </button>
+      )
+    } 
+    else if(doubt.status === "attended"){
+      return(
+        <button className="btn btn-success btn-sm" type="button"
+        onClick={(e)=>{updateDoubtStatus("resolved")}}>
+        <i class="fa-solid fa-circle-check"></i> Mark Resolved
+        </button>
+      )
+    }
+  }
   return (
     <div className={`card doubt-card ${doubt.status}-doubt`}>
       <h6 className="text-center">{COURSE_NAME_MAP[doubt.courseName]}</h6>
-      <span className={`doubt-status-badge ${doubt.status}-bg`}>{doubt.status}</span>
+      <span className={`doubt-status-badge ${doubt.status}-bg`}>
+        {doubt.status}
+      </span>
       <p>
         <h5>{doubt.title}</h5>
         {doubt.description}
       </p>
       <hr />
       <div className="row">
-        <div className='col-md-4'><i class="fa-solid fa-user-graduate"></i> {doubt.user.fullName}</div>
-        <div className='col-md-4'><i class="fa-solid fa-phone"></i> {doubt.user.mobile}</div>
-        <div className='col-md-4'><i class="fa-solid fa-envelope"></i> {doubt.user.email}</div>
+        <div className="col-md-4">
+          <i class="fa-solid fa-user-graduate"></i> {doubt.user.fullName}
+        </div>
+        <div className="col-md-4">
+          <i class="fa-solid fa-phone"></i> {doubt.user.mobile}
+        </div>
+        <div className="col-md-4">
+          <i class="fa-solid fa-envelope"></i> {doubt.user.email}
+        </div>
       </div>
-      <hr />
-      <div className='d-flex justify-content-around'>
-        <button className='btn btn-warning btn-sm'><i class="fa-solid fa-video"></i> Mark Attended</button>
-        <button className='btn btn-success btn-sm'><i class="fa-solid fa-circle-check"></i> Mark Resolved</button>
+      {doubt.status !== "resolved" &&  <hr />}
+      <div className="d-flex justify-content-around">
+        <GetStatusButton />
       </div>
     </div>
-  )
+  );
 }
 
-export default TADoubtCard
+export default TADoubtCard;
--- server/index.js
+++ server/index.js
@@ -1,115 +1,119 @@
-const express = require('express');
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const path = require('path');
-const mongoose = require('mongoose')
-const User = require('./models/User')
-const Doubt = require('./models/Doubt');
-const TeachingAssistant = require('./models/TeachingAssistant');
-const { default: axios } = require('axios');
-const getTAEmail = require('./utils/getTAEmail');
-const { response } = require('express');
+const express = require("express");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const Doubt = require("./models/Doubt");
+const TeachingAssistant = require("./models/TeachingAssistant");
+const { default: axios } = require("axios");
+const getTAEmail = require("./utils/getTAEmail");
+const { response } = require("express");
 
 const app = express();
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-  console.log('Connected to DB 📦');
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to DB 📦");
+  }
+);
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "All Good 👍",
+  });
 });
 
-app.get('/health', (req, res) => {
-  res.json({
-    status: 'All Good 👍'
-  })
-})
-
-app.post('/user', async (req, res) => {
-  const { fullName, email, mobile } = req.body
+app.post("/user", async (req, res) => {
+  const { fullName, email, mobile } = req.body;
 
   if (!fullName) {
     return res.send({
       success: false,
-      message: 'fullName cannot be empty'
-    })
+      message: "fullName cannot be empty",
+    });
   }
 
   if (!email) {
     return res.send({
       success: false,
-      message: 'email cannot be empty'
-    })
+      message: "email cannot be empty",
+    });
   }
 
   if (!mobile) {
     return res.send({
       success: false,
-      message: 'mobile cannot be empty'
-    })
+      message: "mobile cannot be empty",
+    });
   }
 
   const user = await User.findOne({
     email: email,
-  })
+  });
 
   if (user) {
-    await User.updateOne({
-      email: email,
-    },
+    await User.updateOne(
+      {
+        email: email,
+      },
       {
         $set: {
           fullName: fullName,
           mobile: mobile,
-        }
-      })
+        },
+      }
+    );
 
     const updatedUser = await User.findOne({
       email: email,
-    })
+    });
 
-    res.send(updatedUser)
-    return
+    res.send(updatedUser);
+    return;
   }
 
   const newUser = new User({
     fullName: fullName,
     email: email,
-    mobile: mobile
-
-  })
+    mobile: mobile,
+  });
 
   const savedUser = await newUser.save();
 
   res.send(savedUser);
-})
+});
 
 app.post("/doubt", async (req, res) => {
-  const { title, description, courseName, slot, status, email } = req.body
+  const { title, description, courseName, slot, status, email } = req.body;
 
   const user = await User.findOne({
-    email
-  })
-  // TODO: if pending doubt is already for user then return pending message 
-  
-  const pendingDobuts = await Doubt.find({
-    user: user, 
-    status: "pending"
-  })
+    email,
+  });
+  // TODO: if pending doubt is already for user then return pending message
 
-  if(pendingDobuts.length>0)
-  {
-   return res.json({ 
-      success: false, 
-      data: [], 
-      message: "You can ask new dobut once pending dobut is resovled" 
-    }) 
+  const pendingDobuts = await Doubt.find({
+    user: user,
+    status: "pending",
+  });
+
+  if (pendingDobuts.length > 0) {
+    return res.json({
+      success: false,
+      data: [],
+      message: "You can ask new dobut once pending dobut is resovled",
+    });
   }
 
   const TAEmail = getTAEmail(courseName);
   const teachingAssistant = await TeachingAssistant.findOne({
-    email: TAEmail
-  })
+    email: TAEmail,
+  });
 
   const newDoubt = new Doubt({
     title,
@@ -119,122 +123,143 @@ app.post("/doubt", async (req, res) => {
     status,
     user: user,
     teachingAssistant: teachingAssistant,
-  })
+  });
   // TODO: send notification to slack channel
-  
-  const response = await axios.post("https://slack.com/api/chat.postMessage", {
-    "channel": "C03N225P5FX",
-    "text": `Hello *${teachingAssistant.fullName}*, New doubt is assigned to you.
+
+  const response = await axios.post(
+    "https://slack.com/api/chat.postMessage",
+    {
+      channel: "C03N225P5FX",
+      text: `Hello *${teachingAssistant.fullName}*, New doubt is assigned to you.
 *${user.fullName}* has asked *${title}*. Doubt Session with him is scheduled at *${slot}*. 
 please call him now to inform about this session *${user.mobile}*.
-More details are avilable in your dashboard. `
-  }, {
-    headers: {
-      authorization: `Bearer ${process.env.AUTH_TOKEN}`
+More details are avilable in your dashboard. `,
+    },
+    {
+      headers: {
+        authorization: `Bearer ${process.env.AUTH_TOKEN}`,
+      },
     }
-  })
+  );
 
   const savedDoubt = await newDoubt.save();
   res.json({
-    success: true, 
-    data: savedDoubt, 
-    message: "New doubt is added successfully" 
-  })
-})
+    success: true,
+    data: savedDoubt,
+    message: "New doubt is added successfully",
+  });
+});
 
-app.get('/doubts', async (req, res) => {
+app.get("/doubts", async (req, res) => {
   const email = req.query.email;
   const user = await User.findOne({
-    email: email
-  })
+    email: email,
+  });
 
-  if(!user) {
-    return res.send([])
+  if (!user) {
+    return res.send([]);
   }
 
   const doubts = await Doubt.aggregate([
     {
       $match: {
-        user: user._id
-      }
+        user: user._id,
+      },
     },
     {
-    $lookup: {
-      from: "teachingassistants",
-      localField: "teachingAssistant",
-      foreignField: "_id",
-      as: "teachingAssistant"
-    }
-  }
+      $lookup: {
+        from: "teachingassistants",
+        localField: "teachingAssistant",
+        foreignField: "_id",
+        as: "teachingAssistant",
+      },
+    },
   ]);
   res.json(doubts);
-})
+});
 
-app.get('/doubtsforta/:email', async (req, res) => {
- const {email}= req.params;
- 
- const teachingAssistant = await TeachingAssistant.findOne({
-    email: email
- })
+app.get("/doubtsforta/:email", async (req, res) => {
+  const { email } = req.params;
 
-  if(!teachingAssistant) {
-    return res.send([])
+  const teachingAssistant = await TeachingAssistant.findOne({
+    email: email,
+  });
+
+  if (!teachingAssistant) {
+    return res.send([]);
   }
 
   const doubts = await Doubt.aggregate([
     {
       $match: {
-        teachingAssistant: teachingAssistant._id
-      }
+        teachingAssistant: teachingAssistant._id,
+      },
     },
     {
       $lookup: {
         from: "users",
         localField: "user",
         foreignField: "_id",
-        as: "user"
-      }
+        as: "user",
+      },
     },
     {
-      $unwind: "$user"
-    }
+      $unwind: "$user",
+    },
   ]);
 
   res.send(doubts);
-})
+});
 
 app.post("/assistant", async (req, res) => {
-  const { fullName, email, mobile, token } = req.body
+  const { fullName, email, mobile, token } = req.body;
 
   const newTeachingAssistant = new TeachingAssistant({
     fullName,
     email,
     mobile,
-    token
-  })
+    token,
+  });
 
   const savedTeachingAssistant = await newTeachingAssistant.save();
   res.send(savedTeachingAssistant);
-})
+});
 
-
-app.get('/assistants', async (req, res) => {
-  const {email, token} = req.query;
+app.get("/assistants", async (req, res) => {
+  const { email, token } = req.query;
   const teachingAssistant = await TeachingAssistant.findOne({
     email: email,
-    token: token
+    token: token,
   });
   res.send({
     success: teachingAssistant ? true : false,
-    data: teachingAssistant
-  })
-})
+    data: teachingAssistant,
+  });
+});
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.post("/updatedoubt", async (req, res) => {
+  const { doubtId, status } = req.body;
+  const doubt = await Doubt.updateOne(
+    {
+      _id: mongoose.Types.ObjectId(doubtId),
+    },
+    {
+      $set: {
+        status: status,
+      },
+    }
+  );
+  res.send({
+    success: true,
+    message: `Doubt is marked as ${status}`,
+  });
+});
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
   });
 }
 
