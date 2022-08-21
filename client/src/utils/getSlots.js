5fee14ea3abdd1b9e37ba1c5a6d855ea4bdd9ec7
Suraj Shende <surajshende247@gmail.com>
2022-08-21 08:49:10 +0530

    feat: get slots util function

---
+++ client/src/utils/getSlots.js
@@ -0,0 +1,30 @@
+const allSlots = [
+  {
+    startHour: 8,
+    title: "8:00 AM - 9:00 AM",
+  },
+  {
+    startHour: 10,
+    title: "10:00 AM - 11:00 AM",
+  },
+  {
+    startHour: 12,
+    title: "12:00 PM - 1:00 PM",
+  },
+  {
+    startHour: 14,
+    title: "2:00 PM - 3:00 PM",
+  },
+  {
+    startHour: 16,
+    title: "4:00 PM - 5:00 PM",
+  },
+  {
+    startHour: 19,
+    title: "7:00 PM - 8:00 PM",
+  },
+];
+
+export default function getSlots(currentHour) {
+  return allSlots.filter((slot) => slot.startHour >= currentHour + 2);
+}
