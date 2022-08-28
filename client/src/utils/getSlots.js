bb057c5ce08865e19428e6640a874b150b5bad2b
Suraj Shende <surajshende247@gmail.com>
2022-08-28 14:32:37 +0530

    ref: time slot and TA

--- client/src/utils/getSlots.js
+++ client/src/utils/getSlots.js
@@ -8,12 +8,20 @@ const allSlots = [
     title: "10:00 AM - 11:00 AM",
   },
   {
+    startHour: 11,
+    title: "11:00 AM - 12:00 PM",
+  },
+  {
     startHour: 12,
     title: "12:00 PM - 1:00 PM",
   },
   {
     startHour: 14,
     title: "2:00 PM - 3:00 PM",
+  },
+  {
+    startHour: 15,
+    title: "3:00 PM - 4:00 PM",
   },
   {
     startHour: 16,
@@ -24,12 +32,12 @@ const allSlots = [
     title: "7:00 PM - 8:00 PM",
   },
   {
-    startHour: 23,
-    title: "11:00 PM - 12:00 PM",
+    startHour: 20,
+    title: "8:00 PM - 9:00 PM",
   }
 ];
 
 export default function getSlots() {
   const currentHour = new Date().getHours()
-  return allSlots.filter((slot) => slot.startHour >= currentHour + 2);
+  return allSlots.filter((slot) => slot.startHour >= currentHour + 1);
 }
